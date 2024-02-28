08b61b1fa70e6ec94f466a84ccfe526bc63a46b3
Киса Мурлыса <kisamurlysa@MacBook-Air-Ola.local>
2024-02-28 13:07:14 +0500

    Исправили конфликты

--- Recipely/Recipely/ProfileScreen/Presenter/ProfilePresenter.swift
+++ Recipely/Recipely/ProfileScreen/Presenter/ProfilePresenter.swift
@@ -3,7 +3,7 @@
 
 import UIKit
 
-///Протокол для презентера экрана профиля
+/// Протокол для презентера экрана профиля
 protocol ProfilePresenterProtocol {
     func changeName(text: String)
     func showTermsPolicy()
---
+++ Recipely/Recipely/TermsPolicyScreen/View/TermsPolicyViewController.swift
@@ -0,0 +1,7 @@
+// TermsPolicyViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Экран условий
+final class TermsPolicyViewController: UIViewController {}
