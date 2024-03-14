eeeb48d2fb9625cc09e5ea0beb4845457aa5d2f4
Киса Мурлыса <kisamurlysa@MacBook-Air-Ola.local>
2024-03-14 21:47:54 +0500

    Пыталась сделать шиммер для детального рецепта

---
+++ Recipely/Recipely/RecipesScreeen/RecipeDetailScreen/Cells/ImageCell/ShimmerImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShimmerImageTableViewCell.swift
+//  Recipely
+//
+//  Created by Киса Мурлыса on 14.03.2024.
+//
+
+import UIKit
+
+class ShimmerImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
